2ad6bc615f1b660fbdd5685c91cd8a019d5fb1b5
the-parthenon <ericinportland@gmail.com>
2022-04-27 16:24:43 -0700

    Button working

--- public/main.js
+++ public/main.js
@@ -1,12 +1,23 @@
 // const emailSubmit = document.getElementById('email-button');
 const emailInput = document.querySelector('input');
 const form = document.querySelector('form#contact');
+const aboutUs = document.getElementById('about-us');
+const aboutClick = document.getElementById('about-click');
 
 const formSubmission = (evt) => {
   evt.preventDefault();
-  console.log('Form submitted.');
+  //   console.log('Form submitted.');
   alert('Great!  Your grandma will be rickrolled within the next 60 working days!');
   emailInput.value = '';
 };
 
+const aboutReveal = () => {
+  aboutUs.classList.remove('hide');
+  setTimeout(() => {
+    aboutUs.classList.add('hide');
+  }, 5000);
+};
+
 form.addEventListener('submit', formSubmission);
+// aboutClick.addEventListener('mouseover');
+aboutClick.addEventListener('click', aboutReveal);
