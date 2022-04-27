3a95aa0b9f824846ae91173dbaec2b755b794c4b
the-parthenon <ericinportland@gmail.com>
2022-04-27 16:28:40 -0700

    CSS link

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ app.get('/js', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/main.js'));
 });
 
+app.get('/css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/styles.css'));
+});
+
 const port = process.env.PORT || 4040;
 
 app.listen(port, () => {
