b279f61ede756b96c910250a50be4412f7acdad6
the-parthenon <ericinportland@gmail.com>
2022-04-27 15:22:35 -0700

    Testing Submission functionality

--- server/index.js
+++ server/index.js
@@ -12,3 +12,15 @@ const port = process.env.PORT || 4040;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+// const emailSubmit = document.getElementById('email-button');
+const emailInput = document.querySelector('input');
+const form = document.querySelector('form#contact');
+
+const formSubmission = (evt) => {
+  evt.preventDefault();
+  alert('Great!  Your grandma will be rickrolled within the next 60 working days!');
+  emailInput.value = '';
+};
+
+form.addEventListener('submit', formSubmission);
